e7b4f59ec8a0a4071c70146de3bfcf832b61e97a
kishan-kareliya <kareliyakishan007@gmail.com>
2024-08-05 10:50:38 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_DOMAIN,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("welcome to movie review api");
