43ff5e31ccfe797fb09d0c73577629cd64cee9db
kishan-kareliya <kareliyakishan007@gmail.com>
2024-08-06 12:52:43 +0530

    fix : handle file deletion (registration) on validation error

--- src/middlewares/userValidation.ts
+++ src/middlewares/userValidation.ts
@@ -1,24 +1,52 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import { z } from "zod";
+import path from "node:path";
+import fs from "fs/promises";
 
 const userRegistrationSchema = z.object({
   username: z.string().min(2, "username atleast 2 characters"),
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "password must be atleast 8 characters"),
-  profileImage: z.string().optional(),
+  profileImage: z.any().optional(),
 });
 
-const validateRegistration = (
+const validateRegistration = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const result = userRegistrationSchema.safeParse(req.body);
+  const { username, email, password } = req.body;
+
+  const result = userRegistrationSchema.safeParse({
+    username,
+    email,
+    password,
+    profileImage: req.file,
+  });
+
   if (!result.success) {
-    const error = createHttpError(400, "validation error");
+    // if validation error then first check user upload profileImage or not
+    const profileImageName = req.file?.filename;
+
+    // if user upload profile picture then deleted from our server
+    if (profileImageName) {
+      try {
+        await fs.unlink(
+          path.resolve(
+            __dirname,
+            `../../public/users/profileImage/${profileImageName}`
+          )
+        );
+      } catch (err) {
+        console.error("Error deleting temporary file:", err);
+      }
+    }
+
+    const error = createHttpError(400, "Validation error");
     return next(error);
   }
+
   next();
 };
 
